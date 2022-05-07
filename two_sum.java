89e0f47d5ac960205b28cd8f9f1678fb808d2c51
Nirvik Basnet <46699486+nirvikBasnet@users.noreply.github.com>
2022-05-07 20:31:02 +1000

    two_sum

---
+++ two_sum.java
@@ -0,0 +1,41 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class two_sum {
+
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4};
+        int target = 6;
+
+        sumOfIndex(arr, target);
+
+
+    }
+
+    public static int[] sumOfIndex(int[] arr, int target){
+
+        int[] result = new int[2];
+
+        Map<Integer,Integer> map = new HashMap<>();
+
+        for(int i = 0; i<arr.length;i++){
+            if(map.containsKey(arr[i])){
+
+                result[0] = i;
+                result[1] = map.get(arr[i]);
+
+
+            }else{
+                map.put(target-arr[i], i);
+            }
+        }
+
+        System.out.println(Arrays.toString(result));
+
+        return result;
+
+    }
+    
+}
