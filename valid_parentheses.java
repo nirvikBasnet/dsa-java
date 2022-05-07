26f9373d6e09a73dfb503cb4868128e7e79710f8
Nirvik Basnet <46699486+nirvikBasnet@users.noreply.github.com>
2022-05-07 22:41:52 +1000

    valid_parentheses

---
+++ valid_parentheses.java
@@ -0,0 +1,43 @@
+import java.util.Stack;
+
+class valid_parentheses{
+    public static void main(String[] args) {
+        String s = "{[()]}";
+
+        isValid(s);
+    }
+
+    public static boolean isValid(String s){
+
+        if(s.length()%2 != 0) return false;
+
+        Stack<Character> stack = new Stack<>();
+
+        for(char c : s.toCharArray()){
+           
+                if( c == '(' || c == '{' || c == '['){
+                    stack.push(c);
+                }else if(c == ')' && !stack.isEmpty() && stack.peek()=='('){
+                    
+                    stack.pop();
+                    
+                }else if(c == '}' && !stack.isEmpty() && stack.peek()=='{'){
+                    
+                    stack.pop();
+                    
+                }else if(c == ']' && !stack.isEmpty() && stack.peek()=='['){
+                    
+                    stack.pop();
+                    
+                } else {
+                    return false;
+                }
+        }
+
+        System.out.println(stack.isEmpty());
+
+
+
+        return stack.isEmpty();
+    }
+}
